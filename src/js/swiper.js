1589e42ee5f790953e6debc1daea0128aea0366a
Lozick <dlozichnyy2005@gmail.com>
2024-05-29 08:17:45 +0700

    add slider

---
+++ src/js/swiper.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+new Swiper('.slider', {
+  navigation: {
+    prevEl: '.slider__button_prev',
+    nextEl: '.slider__button_next',
+  },
+  spaceBetween: 20,
+});
